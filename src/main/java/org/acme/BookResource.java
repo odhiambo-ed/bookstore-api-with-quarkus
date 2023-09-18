481e6e5697c706b7d2efb9dd3aa4eec287b5832b
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-18 20:06:56 +0300

    add book get method

--- src/main/java/org/acme/BookResource.java
+++ src/main/java/org/acme/BookResource.java
@@ -1,5 +1,22 @@
 package org.acme;
 
+import java.util.List;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/v1/books")
+@Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
-    
+    @GET
+    public List<Book> getAllBooks() {
+        return List.of(
+            new Book(1, "Java for Beginers", "Edward", 2023, "Java"),
+            new Book(1, "JavaScript for Beginers", "Edward", 2023, "JavaScript"),
+            new Book(1, "Ruby for Beginers", "Edward", 2023, "Ruby"),
+            new Book(1, "React for Beginers", "Edward", 2023, "React")
+        );
+    }
 }
